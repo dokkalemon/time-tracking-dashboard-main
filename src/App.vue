5f187081cb9346eacd66847da5b38170872b0ad8
dokkalemon <dottori.roberto@gmail.com>
2022-01-03 21:54:41 +0100

    add user card container

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,36 @@
 <template>
-  <h1>Reset</h1>
+  <main>
+    <UserCard />
+
+
+  </main>
+  
+
+
+
 </template>
 
 <script>
+import UserCard from '@/components/UserCard.vue'
 
 export default {
   name: 'App',
   components: {
+    UserCard
 
   }
 }
 </script>
 
 <style lang="scss">
-@import '@/styles/global.scss'
+@import '@/styles/global.scss';
 
-
+main {
+  height: 100vh;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
 
 </style>
---
+++ src/components/UserCard.vue
@@ -0,0 +1,33 @@
+<template>
+    <section class="user-card">
+
+
+
+
+
+
+    </section>
+</template>
+
+
+
+
+<script>
+export default {
+    name: 'UserCard'
+}
+</script>
+
+<style scoped lang="scss">
+@import '@/styles/var.scss';
+
+    .user-card {
+        height: 480px;
+        width: 235px;
+        background-color: $front-card;
+        border-radius: 15px;
+        box-shadow: 5px 5px 15px rgba(0, 0, 0, 0.262);
+    }
+
+
+</style>
